97981e0aca02dd26c3087d14bdd75ba84a2c34af
Verner Fortelius <verner@fortelius.net>
2024-01-01 23:15:37 +0200

    path client, kinda

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use azure_storage::StorageCredentials;
-use azure_storage_datalake::{self, clients::DataLakeClient};
-use futures::stream::StreamExt;
-use std::iter::Iterator;
-use std::sync::{Arc, Mutex};
-use tokio::task::JoinSet;
+use azure_rust_tinkering::PathClient;
+use azure_storage_datalake::file_system::Path;
+use azure_storage_datalake::{self};
+use std::sync::Arc;
+use tokio::sync::mpsc;
 extern crate dotenv;
 use dotenv::dotenv;
 use std::env;
@@ -14,53 +13,43 @@ async fn main() -> azure_core::Result<()> {
 
     let account = env::var("ACCOUNT").expect("Datalake account name seems to be missing...");
     let sas_token = env::var("SAS_TOKEN").expect("Guess the sas token is missing...");
+    let file_system_name =
+        env::var("FILE_SYSTEM_NAME").expect("Dont forget file system name either...");
 
-    let paths = Arc::new(Mutex::new(Vec::new()));
-    let mut file_system_tasks = JoinSet::new();
+    let (paths_sender, mut paths_receiver) = mpsc::channel::<Option<Path>>(10000);
+    let paths_sender = Arc::new(paths_sender);
 
-    let data_lake_client = Arc::new(DataLakeClient::new(
-        account,
-        StorageCredentials::sas_token(sas_token).expect("hu? check your token"),
-    ));
+    let path_client = PathClient::new(account.to_string(), sas_token.to_string());
 
-    let mut filesystems = data_lake_client.list_file_systems().into_stream();
+    let read_task = tokio::spawn(async move {
+        let mut read_count = 0;
+        loop {
+            match paths_receiver.recv().await {
+                Some(_) => {
+                    read_count += 1;
 
-    while let Some(fs_response) = filesystems.next().await {
-        fs_response.unwrap().into_iter().for_each(|f| {
-            let client = Arc::clone(&data_lake_client);
-            let pathssss = Arc::clone(&paths);
-
-            file_system_tasks.spawn(async move {
-                let file_system_client = client.file_system_client(f.name.to_string());
-                println!("Listing files in path {} started", f.name);
-
-                let mut paths = file_system_client
-                    .list_paths()
-                    .recursive(false)
-                    .into_stream();
-
-                while let Some(path_response) = paths.next().await {
-                    pathssss
-                        .lock()
-                        .unwrap()
-                        .append(&mut path_response.unwrap().paths);
+                    if read_count % 1000 == 0 {
+                        // println!("Read {} files so far", read_count);
+                    }
                 }
-            });
-        })
-    }
+                None => break,
+            }
+        }
 
-    while let Some(_) = file_system_tasks.join_next().await {
-        // eh.. its fine
-    }
+        println!("Read all paths \\o/");
+    });
 
-    Arc::try_unwrap(paths)
-        .unwrap()
-        .into_inner()
-        .unwrap()
-        .iter()
-        .for_each(|path| {
-            println!("{}", path.name);
-        });
+    let count = path_client
+        .list_paths_parallel(
+            file_system_name.to_string(),
+            "/".to_string(),
+            paths_sender,
+            256,
+        )
+        .await;
 
+    read_task.await.expect("durr...");
+
+    println!("Found {} files", count);
     Ok(())
 }
